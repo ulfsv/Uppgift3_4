991d70f67ad16f57dbc0127e60936daa80da540c
ulfsv <ulf.e.sv@gmail.com>
2020-09-09 16:33:20 +0200

    Add project files.

---
+++ Uppgift3_4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uppgift3_4
+
+//11. F: Varför är polymorfism viktigt att behärska?
+//    S: Det är viktigt för att undvika att duplicera kod och att kunna återanvända den.
+//12. F: Hur kan polymorfism förändra och förbättra kod via en bra struktur?
+//    S: Bättre struktur, blir lättare att begripa och överblicka koden.
+//13. F: Vad är det för en skillnad på en Abstrakt klass och ett Interface?
+//    S: En abstrakt klass är en klass som man inte kan instansiera.
+//       Ett interface är ett API till ett objekt. Man kan implementera ett interface 
+//       hur många gånger man vill men en abstrakt klass kan bara ärva från en basklass.
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserError> ue = new List<UserError>();
+            ue.Add(new NumericInputError());
+            ue.Add(new TextInputError());
+            ue.Add(new NumericInputStatus());
+            ue.Add(new TextToLongError());        
+
+            foreach (UserError userError in ue)
+            {
+                Console.WriteLine(userError.UEMessage());
+            }
+        }
+    }
+}
---
+++ Uppgift3_4/UserError.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift3_4
+{
+    abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+
+    class NumericInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a numeric input in a text only field. This fired an error!";
+        }
+    }
+    class TextInputError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You tried to use a text  input in a numeric only field. This fired an error!";
+        }
+    }
+    class NumericInputStatus : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Numeric input is ongoing";
+        }
+    }
+
+    class TextToLongError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "Text is too long.";
+        }
+    }
+}
